d73eea5ddf5f20bdf91ed0c9030c66ff4541641f
JonasHWL <hwljonas@gmail.com>
2024-06-04 16:33:43 +0200

    fix 2.0

--- src/main/java/com/example/line/LineE.java
+++ src/main/java/com/example/line/LineE.java
@@ -11,7 +11,7 @@ import java.util.Random;
 public class LineE {
     private final Random randSeed = new Random();
     //private final int seed = randSeed.nextInt();
-    private final int seed = 123; //Hardcoded seed als Beispiel
+    private final long seed = 9223372036854775807L; //Hardcoded seed als Beispiel
     private final Random rand = new Random(seed);
     /**
      * ArrayList which stores all Points
@@ -41,7 +41,7 @@ public class LineE {
         for (int i = 0; i < 5; i++){
             int xPos = rand.nextInt(1, 15);
             int yPos = rand.nextInt(1,15);
-            pointsList.add(new Points(xPos*20, yPos*20));
+            pointsList.add(new Points(xPos*40, yPos*40));
         }
 
         for (int i = 0; i < pointsList.size(); i++) {
